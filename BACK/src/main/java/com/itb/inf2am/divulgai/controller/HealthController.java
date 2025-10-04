6d7379e784deb5317db7a6036012011d14ef0f8e
miguelsene <rm95370@estudante.fieb.edu.br>
2025-10-03 22:21:04 -0300

    esta 90% conectado ao banco (falta um teco de confirmaçã)

--- BACK/src/main/java/com/itb/inf2am/divulgai/controller/HealthController.java
+++ BACK/src/main/java/com/itb/inf2am/divulgai/controller/HealthController.java
@@ -1,3 +1,10 @@
+package com.itb.inf2am.divulgai.controller;
+import
+org.springframework.web.bind.annotation.GetMapping;
+import
+org.springframework.web.bind.annotation.RestController;
+import
+org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping ("/api")
 public class HealthController{
